d62294f19120e85d5fb373c082019dd8dffb2621
Dawson R <1885328+ddaws@users.noreply.github.com>
2026-01-08 11:46:23 +0800

    add a mock erc20 token

---
+++ test/mocks/MockERC20.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.30;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/**
+ * @title MockERC20
+ * @notice Simple ERC20 token for testing (used by LockToken tests)
+ */
+contract MockERC20 is ERC20 {
+    constructor() ERC20("Mock Token", "MOCK") {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public {
+        _burn(from, amount);
+    }
+}
