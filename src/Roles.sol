d1c9c7bcd7d62b0bbab4ce44368e21945b6e42d9
Dawson <1885328+ddaws@users.noreply.github.com>
2026-01-27 20:36:11 +0800

    make ApyUSD use a new ERC20DenyListUpgradable

--- src/Roles.sol
+++ src/Roles.sol
@@ -88,18 +88,15 @@ library Roles {
      * @param apyUSD The ApyUSD contract
      */
     function assignAdminTargetsFor(AccessManager self, ApyUSD apyUSD) internal {
-        bytes4[] memory selectors = new bytes4[](9);
+        bytes4[] memory selectors = new bytes4[](7);
         selectors[0] = ApyUSD.pause.selector;
         selectors[1] = ApyUSD.unpause.selector;
 
-        selectors[2] = ApyUSD.freeze.selector;
-        selectors[3] = ApyUSD.unfreeze.selector;
-
-        selectors[4] = ApyUSD.setDenyList.selector;
-        selectors[5] = ApyUSD.setUnlockToken.selector;
-        selectors[6] = ApyUSD.setVesting.selector;
-        selectors[7] = ApyUSD.setUnlockingFee.selector;
-        selectors[8] = ApyUSD.setFeeWallet.selector;
+        selectors[2] = ApyUSD.setDenyList.selector;
+        selectors[3] = ApyUSD.setUnlockToken.selector;
+        selectors[4] = ApyUSD.setVesting.selector;
+        selectors[5] = ApyUSD.setUnlockingFee.selector;
+        selectors[6] = ApyUSD.setFeeWallet.selector;
         self.setTargetFunctionRole(address(apyUSD), selectors, ADMIN_ROLE);
     }
 
